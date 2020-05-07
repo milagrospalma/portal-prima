7685001c6a7ebe8e09525c45cc62c78d0002c174
Milagros Palma <mnpalma.2@gmail.com>
2020-05-06 22:58:29 -0500

    header

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// La funcionalidad de tu proyecto
+$('#modalLogin').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+
+$('#modalRegister').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+
+$('#modalDrive').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
